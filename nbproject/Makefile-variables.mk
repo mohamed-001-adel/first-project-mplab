a0c16193d4871c72054286b33a1025549fd9e001
mohamed-001-adel <mohamed.abdelmoniem001@gmail.com>
2021-09-11 13:23:57 +0200

    first commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=New_Folder.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/New_Folder.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=newfolder.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/newfolder.tar
---
+++ newavr-main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   newavr-main.c
+ * Author: pc
+ *
+ * Created on 11 ??????, 2021, 01:22 ?
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
